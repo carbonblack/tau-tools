2b487b95c58f54fa4b98f6fe63f2c1026430bab9
Adam Nadrowski <infosecadam@gmail.com>
2020-10-14 19:50:59 -0400

    initial version 2.0 commit

--- navgen/navgen.py
+++ navgen/navgen.py
@@ -2,31 +2,37 @@ import requests
 import json
 import re
 import time
+from pick import pick
 
-# we need to download the json of the mitre attack matrix
-resp = requests.get("https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json")
-data = resp.json()
+def get_label(option):
+    return option.get('label')
+
+def get_product():
+    select = '[!] Select the product: '
+    options = [{'label': 'VMWare Carbon Black EDR (formerly CB Response)'}, {'label': 'VMWare Carbon Black Enterprise EDR (formerly CB ThreatHunter)'}]
+    product = pick(options, select, indicator='*', options_map_func=get_label)
+    if product[1] == 0:
+        product = "edr"
+    elif product[1] == 1:
+        product = "eedr"
+    else:
+        return None
+
+    return product
 
 def get_auth():
-    """this will ask for auth info to CbR instance and return it"""
-    print("[!] To find your API key, login to your Cb Response UI and navigate to the profile section.")
+    """this will ask for auth info and return it"""
 
-    full_url = input("[!] Here, enter the full url of your Cb Response instance. Example: https://bugcrowd.my.carbonblack.io\n[*] > ")
+    full_url = input("[!] Here, enter the full url of your instance. Examples: \n\thttps://bugcrowd.my.carbonblack.io\n\thttps://defense-prod05.conferdeploy.net\n\n[*] > ")
     if "https://" not in full_url:
         full_url = "https://" + full_url
 
-    while True:
-        api_key = input("[*] Enter your API key: > ")
-        if len(api_key) != 40:
-            print("[!] Invalid token. Try again.")
-            continue
-        else:
-            break
+    api_key = input("[*] Enter your API key/token: > ")
 
-    return api_key, full_url
+    return full_url, api_key
 
 
-def download_reports(api_key, url):
+def download_edr_reports(url, api_key):
     """
     This will download threat reports for following feeds:
         attackframework
@@ -75,215 +81,189 @@ def download_reports(api_key, url):
     return data
 
 
-def reports_stats(data):
-    """ check to see if feeds are all enabled, and report """
-    list_of_ignored = []
-    for report in data:
-        if report['is_ignored'] is True:
-            list_of_ignored.append(report['id'])
-    if list_of_ignored:
-        print("\n[!] Heads up! The following report ID's are ignored within your Threat Intelligence page. Your final output may be affected.")
-        for ids in list_of_ignored:
-            print("\t{}".format(ids))
-
-
-def hardcoded_tids(tid, comment):
-    """ Some threat reports aren't tagged with a TID. """
-    my_new_list =[] 
-    if get_tactic(data, tid.lower()) is not None:
-        for tactic in get_tactic(data, tid.lower()):
-            my_new_list.append(create_dict(tid, 100, comment, tactic))
-
-    return my_new_list
-
-
-def create_dict(tid, score, comment, tactic):
-    """ this will accept specific parameters and return a dictionary of a
-    specific technique. It will be appended to the "techniques" key in
-     Navigator JSON. """
-
-    color = get_color(score)
-
-    nav_techniques = {
-        "techniqueID": tid.upper(),
-        "tactic": tactic,
-        "color": color,
-        "comment": comment,
-        "enabled": True
+def get_eedr_feed_ids(backend_url, api_key, org_key):
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Auth-Token': api_key
     }
+    # this will create a full url to get total number of reports.
+    full_url = backend_url + "/threathunter/feedmgr/v2/orgs/{}/feeds?include_public=true".format(org_key)
 
-    return nav_techniques
-
-
-def get_tactic(data, tid):
-    """ this should return a list of tactics like ['persistence'] """    
-    objects = data['objects']
-    for object in objects:
-        # print(object)
-        if object['type'] == "attack-pattern":
-            tid2 = object['external_references'][0]['external_id'].lower()
-            if tid == tid2:
-                tactic_list = []
-                for kill_chain_phase in object['kill_chain_phases']:
-                    tactic = kill_chain_phase['phase_name']
-                    tactic_list.append(tactic)
     try:
-        return tactic_list
+        r = requests.get(full_url, headers=headers)
     except:
-        pass
+        print("requests failed to get feed ids")
+    data = r.json()
+    feed_ids = []
+    for feed in data['results']:
+        if feed['access'] == 'public':
+            feed_ids.append(feed['id'])
+
+    return feed_ids
 
 
-def generate_tid_dict(threat_reports):
-    """this will return a dictionary with TID's as keys. Values will be a list of all threat reports for that TID"""
-    tid_dict = {}
-    pattern = "t\d{4}"
+def download_eedr_feed_reports(backend_url, api_key, org_key, feed_id):
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Auth-Token': api_key
+    }
+    # this will create a full url to get total number of reports.
+    full_url = backend_url + "/threathunter/feedmgr/v2/orgs/{}/feeds/{}".format(org_key, feed_id)
 
-    for threat_report in threat_reports:
-        # grab all the supported OS tags
-        if "windows" in threat_report['tags'] or "linux" in threat_report['tags'] or "macos" in threat_report['tags']:
-            for tag in threat_report['tags']:
-                if re.match(pattern, tag):
-                    if tag in tid_dict:
-                        tid_dict[tag].append(threat_report)
-                    else:
-                        tid_dict[tag] = [threat_report]
+    r = requests.get(full_url, headers=headers)
+    data = r.json()
 
-    return tid_dict
+    return data
 
 
 def build_navigator():
-    navigator = {
-        "name": "Cb Response Coverage (Windows,Linux,macOS)",
-        "version": "2.2",
-        "domain": "mitre-enterprise",
-        "description": "",
-        "filters": {
-            "stages": [
-                "act"
-            ],
-            "platforms": [
-                "windows",
-                "mac",
-                "linux"
-            ]
-        },
-        "sorting": 0,
-        "viewMode": 0,
-        "hideDisabled": False,
-        "gradient": {
-            "colors": [
-                "#ff6666",
-                "#ffe766",
-                "#8ec843"
-            ],
-            "minValue": 0,
-            "maxValue": 100
-        },
-        "legendItems": [],
-        "showTacticRowBackground": False,
-        "tacticRowBackground": "#dddddd",
-        "selectTechniquesAcrossTactics": True
-    }
+   navigator  = {
+      "name": "VMWare Carbon Black MITRE ATT&CK Coverage",
+      "version": "3.0",
+      "description": "This layer shows techniques stored in VMWare Carbon Black Threat Intelligence feeds.",
+      "domain": "mitre-enterprise",
+      "legendItems": [],
+      "techniques": []
+   }
 
-    return navigator
+   return navigator
 
 
-def prepare_nav_techniques(tid, threat_report_values):
-    """should accept one tid from build_navigator()... will contain list of mutiple queries."""
+def generate_tid_dict(threat_reports):
+   """this will return a dictionary with TID's as keys. Values will be a list of all threat reports for that TID"""
+   tid_dict = {}
+   pattern = "t\d{4}"
 
-    tactic = get_tactic(data, tid)
-    color = get_color(threat_report_values)
-    comment = get_comment(threat_report_values)
-    nav_technique = {
-        "techniqueID": tid.upper(),
-        "tactic": tactic,
-        "color": color,
-        "comment": comment,
-        "enabled": True
-    }
+   for threat_report in threat_reports:
+      if threat_report['tags'] != None:
+         for tag in threat_report['tags']:
+            if re.match(pattern, tag):
+               if tag in tid_dict:
+                  tid_dict[tag].append(threat_report)
+               else:
+                  tid_dict[tag] = [threat_report]
 
-    return nav_technique
-
-
-def get_comment(threat_report_values):
-    number_of_reports = len(threat_report_values)
-    comment = "There are %s queries matching this TID.\n\n" % (int(number_of_reports))
-    for threat_report in threat_report_values:
-        comment += "Feed: %s\nTitle: %s\nID: %s\nDescription: %s\n\n" % (threat_report['feed_name'], threat_report['title'], threat_report['id'], threat_report['description'].split("\n")[0])
-
-    return comment
+   return tid_dict
 
 
-def get_color(threat_report_value):
-    color_dict = {
-        'green-high': '#00ff61',
-        'green-med': '#83fcb1',
-        'green-low': '#d6ffe5'
-    }
+def prepare_nav_techniques(tid, threat_report_values, product):
+   """should accept one tid from build_navigator()... will contain list of mutiple queries."""
 
-    # this if statement supports hardcoded tids via hardcoded_tids()
-    if type(threat_report_value) == int:
-        score = threat_report_value
-        threat_report_value = []
-        threat_report_value.append({'score': score})
+   color = get_color(threat_report_values, product)
+   description = get_description(threat_report_values)
+   nav_technique = {
+      "techniqueID": tid.upper(),
+      "color": color,
+      "comment": description,
+      "enabled": True
+   }
 
-    for threat_report in threat_report_value:
-        if threat_report['score'] >= 80:
-            return color_dict['green-high']
-        elif threat_report['score'] >= 50:
-            return color_dict['green-med']
-        elif threat_report['score'] >= 0:
-            return color_dict['green-low']
+   return nav_technique
+
+
+def get_color(threat_report_value, product):
+   color_dict = {
+      'green-high': '#00ff61',
+      'green-med': '#83fcb1',
+      'green-low': '#d6ffe5'
+   }
+
+   # this if statement supports hardcoded tids via hardcoded_tids()
+   if type(threat_report_value) == int:
+      severity = threat_report_value
+      threat_report_value = []
+      threat_report_value.append({'severity': severity})
+
+   for threat_report in threat_report_value:
+      if product == 'edr':
+         severity = threat_report['severity'] / 10
+      elif product == 'eedr':
+         severity = threat_report['severity']
+
+      if severity >= 8:
+         return color_dict['green-high']
+      elif severity >= 5:
+         return color_dict['green-med']
+      elif severity >= 0:
+         return color_dict['green-low']
+
+
+def get_description(threat_report_values):
+   number_of_reports = len(threat_report_values)
+   comment = "There are %s queries matching this TID.\n\n" % (int(number_of_reports))
+   for threat_report in threat_report_values:
+      comment += "Feed: %s\nTitle: %s\nID: %s\nDescription: %s\n\n" % (
+      threat_report['feed_name'], threat_report['title'], threat_report['id'],
+      threat_report['description'].split("\n")[0])
+
+   return comment
 
 
 def main():
-    api_key, url = get_auth()
-    print("\nYour url is: {}".format(url))
-    print("Your API key is: {}".format(api_key))
+   product = get_product()
+   if product == None:
+       print("Invalid product. Qutting.")
+       quit(1)
 
-    # download all reports from Cb Response server as json object
-    data_from_cbr = download_reports(api_key, url)
+   backend_url, api_key = get_auth()
+   if product == 'eedr':
+       org_key =  input("[*] Enter your org key: > ")
+   print("\nYour url is: {}".format(backend_url))
+   print("Your API key is: {}".format(api_key))
+   if product == 'eedr':
+       print("Your org key is: {}".format(org_key))
 
-    # provide stats about threat reports found.
-    reports_stats(data_from_cbr)
+   if product == 'edr':
+       reports = download_edr_reports(backend_url, api_key)
+       all_reports = []
+       for report in reports:
+          threat_report = {}
+          threat_report['feed_name'] = report['feed_name']
+          threat_report['id'] = report['id']
+          threat_report['title'] = report['title']
+          threat_report['tags'] = report['tags']
+          threat_report['severity'] = report['score']
+          threat_report['description'] = report['description']
+          all_reports.append(threat_report)
 
-    tid_dict = generate_tid_dict(data_from_cbr)
+   elif product == 'eedr':
+       eedr_feed_ids = get_eedr_feed_ids(backend_url, api_key, org_key)
+       all_reports = []
+       for feed_id in eedr_feed_ids:
+           reports = download_eedr_feed_reports(backend_url, api_key, org_key, feed_id)
+           feed_name = reports['feedinfo']['name']
 
-    nav_techniques_list = []
-    for tid, threat_report_values in tid_dict.items():
-        nav_techniques = prepare_nav_techniques(tid, threat_report_values)
+           for report in reports['reports']:
+               threat_report = {}
+               threat_report['feed_name'] = feed_name
+               threat_report['id'] = report['id']
+               threat_report['title'] = report['title']
+               threat_report['tags'] = report['tags']
+               threat_report['severity'] = report['severity']
+               threat_report['description'] = report['description']
+               all_reports.append(threat_report)
 
-        # Since prepare_nav_techniques returns list with multiple tactics, we should iterate through them to create 1:1 relationships. This for loop fixes that.
-        try:
-            for tactic in nav_techniques['tactic']:
+   # now we have a list containing all reports... we need to identify ones with a mitre attack TID.
+   attack_techniques = []
+   tid_dict = generate_tid_dict(all_reports)
+   navigator = build_navigator()
 
-                nav_techniques_copy = dict(nav_techniques)
-                nav_techniques_copy['tactic'] = tactic
-                nav_techniques_list.append(nav_techniques_copy)
-        except:
-                print("\n[!] WARNING: Did not find tactic for: ")
-                print(nav_techniques)
+   nav_techniques_list = []
+   for tid, threat_report_values in tid_dict.items():
+      nav_techniques = prepare_nav_techniques(tid, threat_report_values, product)
+      navigator['techniques'].append(nav_techniques)
 
-    navigator = build_navigator()
-    navigator['techniques'] = nav_techniques_list
+   # builds the navigator json
+   navigator_json = json.dumps(navigator, indent=4, sort_keys=True)
 
-    # we'll hardcode some TID's that target a fundamental feature.
-    # extending the existing list in case a hard coded tid has more than one tactic. This will unroll the list from hardcoded_tids()
-    navigator['techniques'].extend(hardcoded_tids("t1129", "Native product functionality."))
-    navigator['techniques'].extend(hardcoded_tids("t1116", "Native product functionality."))
-    navigator['techniques'].extend(hardcoded_tids("t1065", "Native product functionality."))
-    navigator['techniques'].extend(hardcoded_tids("t1043", "Native product functionality."))
-
-    # builds the navigator json
-    navigator_json = json.dumps(navigator, indent=4, sort_keys=True)
-
-    # save to disk in current working dir
-    filename = "CbResponseNavigator-" + str(int(time.time())) + ".json"
-    with open(filename, 'w') as outfile:
-        outfile.write(navigator_json)
-        print("\n[!] Saved MITRE Navigator json file as " + filename)
-        print("[!] Use this file to 'Open Existing Layer' from local file on https://mitre.github.io/attack-navigator/enterprise/")
+   # save to disk in current working dir
+   filename = "VMWareCBThreatIntel-{}-".format(product.upper()) + str(int(time.time())) + ".json"
+   with open(filename, 'w') as outfile:
+      outfile.write(navigator_json)
+      print("\n[!] Saved MITRE Navigator json file as " + filename)
+      print(
+         "[!] Use this file to 'Open Existing Layer' from local file on https://mitre.github.io/attack-navigator/enterprise/")
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+   main()
